721ee8a35ead963226f5e6adb78217a3cd063fd7
nadir inab <nadir.inab.dev@gmail.com>
2023-05-22 17:30:19 +0100

    data is well done

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -12,86 +12,103 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[Route(path: "/api/users", name: "app_user")]
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user', methods:['GET'])]
-    #[Route('/', name: 'index', methods:['GET'])]
-    public function index(): JsonResponse
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function index(EntityManagerInterface $em): JsonResponse
     {
+        $users = $em->getRepository(User::class)->findAll();
+        $data = [];
+
+        foreach ($users as $user) {
+            $data[] = [
+                'id' => $user->getId(),
+                'name' => $user->getName(),
+                'age' => $user->getAge(),
+                'email' => $user->getEmail(),
+                'password' => $user->getPassword()
+            ];
+        }
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
+            'users' => $data
         ]);
     }
 
-    #[Route('/store', name:'user_store', methods:['POST'])]
+    #[Route('/store', name: 'user_store', methods: ['POST'])]
     public function store(Request $request, EntityManagerInterface $em): JsonResponse
-    {   
-        $data = $request->request->all() ;
-
-        $name = $request->request->get('name') ;
-        $email = $request->request->get('email') ;
-        $password = $request->request->get('password') ;
-        $age = $request->request->get('age') ;
-        $user = new User ;
-        $user->setName($name) ;
-        $user->setEmail($email) ;
-        $user->setPassword($password) ;
-        $user->setAge($age) ;
-        $em->persist($user) ;
-        $em->flush() ;
+    {
+        $user = new User;
+        $user->setName($request->request->get('name'));
+        $user->setEmail($request->request->get('email'));
+        $user->setPassword($request->request->get('password'));
+        $user->setAge($request->request->get('age'));
+        $em->persist($user);
+        $em->flush();
+        $data = [
+            'id' => $user->getId(),
+            'name' => $user->getName(),
+            'email' => $user->getEmail(),
+            'password' => $user->getPassword(),
+            'age' => $user->getAge()
+        ];
         return new JsonResponse([
-            "user" => $user
-        ]) ;
+            "user" => $data
+        ]);
     }
 
-    #[Route('/show/{id}', name:"user_show", methods:['GET'])]
-    public function show(int $id, EntityManagerInterface $em): JsonResponse 
+    #[Route('/{id}', name: "user_show", methods: ['GET'])]
+    public function show(int $id, EntityManagerInterface $em): JsonResponse
     {
         $user = $em->getRepository(User::class)->find($id);
-        if(!$user){
-            return $this->json("No user is found for id".$id, 404) ;
+        if (!$user) {
+            return $this->json("No user is found for id: " . $id, 404);
         }
         $data = [
-            "id" => $user->getId() ,
+            "id" => $user->getId(),
             "name" => $user->getEmail(),
             "age" => $user->getAge(),
             "password" => $user->getPassword()
+        ];
+
+        return new JsonResponse([
+            "user" => $data
+        ]);
+    }
+
+
+    #[Route('/{id}', name: 'edit_user', methods: ['PUT'])]
+    public function edit(Request $request, int $id, EntityManagerInterface $em): JsonResponse
+    {
+        $user = $em->getRepository(User::class)->find($id);
+        if (!$user) {
+            return $this->json("No User found for id :" . $id, 404);
+        }
+        $user->setName($request->request->get('name'));
+        $user->setEmail($request->request->get('email'));
+        $user->setAge($request->request->get('age'));
+        $user->setPassword($request->request->get('password'));
+        $em->flush();
+        $data = [
+            'id' => $user->getId(), 
+            'name' => $user->getName(),
+            'email' => $user->getEmail(), 
+            'password' => $user->getPassword()
         ] ;
 
         return new JsonResponse([
             "user" => $data
-        ]) ;
+        ]);
     }
 
-
-    #[Route('edit/{id}', name:'edit_user', methods:['PUT'])]
-    public function edit(Request $request,int $id, EntityManagerInterface $em): JsonResponse
+    #[Route('/{id}', name: 'user_delete', methods: ['DELETE'])]
+    public function delete(int $id, EntityManagerInterface $em): JsonResponse
     {
         $user = $em->getRepository(User::class)->find($id);
-        if(!$user){
-            return $this->json("No User found for id :".$id, 404) ;
-        }
-        $user->setName($request->request->get('name')) ;
-        $user->setEmail($request->request->get('email')) ;
-        $user->setAge($request->request->get('age')) ;
-        $user->setPassword($request->request->get('password')) ;
-        $em->flush() ;
-
+        if (!$user) {
+            return $this->json("No user found for id :" . $id, 404);
+        };
+        $em->remove($user);
+        $em->flush();
         return new JsonResponse([
             "user" => $user
-        ]) ;
-    }
-
-    #[Route('user/{id}', name:'user_delete', methods:['DELETE'])]
-    public function delete(int $id,EntityManagerInterface $em): JsonResponse
-    {
-        $user = $em->getRepository(User::class)->find($id) ;
-        if(!$user){
-            return $this->json("No user found for id :".$id, 404) ;
-        } ;
-        $em->remove($user) ;
-        $em->flush() ;
-        return new JsonResponse([
-            "user" => $user
-        ]) ;
+        ]);
     }
 }
